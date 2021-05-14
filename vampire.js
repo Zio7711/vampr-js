8d457ed1d25ee7acd16a804dcdab496ca7c3adc5
Zio <zi.ao.tan@outlook.com>
2021-05-14 23:09:55 +0000

    finish stretch closest commom ancestor

--- vampire.js
+++ vampire.js
@@ -10,22 +10,32 @@ class Vampire {
 
   // Adds the vampire as an offspring of this vampire
   addOffspring(vampire) {
-
+    this.offspring.push(vampire);
+    vampire.creator = this;
   }
 
   // Returns the total number of vampires created by that vampire
   get numberOfOffspring() {
-
+    return this.offspring.length;
   }
 
   // Returns the number of vampires away from the original vampire this vampire is
   get numberOfVampiresFromOriginal() {
+    let result = 0;
+    let vampire = this;
 
+    while (vampire.creator) {
+      result++;
+      vampire = vampire.creator;
+    }
+    return result;
   }
 
   // Returns true if this vampire is more senior than the other vampire. (Who is closer to the original vampire)
   isMoreSeniorThan(vampire) {
-
+    return (
+      this.numberOfVampiresFromOriginal < vampire.numberOfVampiresFromOriginal
+    );
   }
 
   /** Stretch **/
@@ -36,9 +46,29 @@ class Vampire {
   // * when comparing Ansel and Sarah, Ansel is the closest common anscestor.
   // * when comparing Ansel and Andrew, Ansel is the closest common anscestor.
   closestCommonAncestor(vampire) {
+    let myAncestors = [this];
+    let ancestors = [vampire];
+    let me = this;
 
+    while (me.creator) {
+      myAncestors.push(me.creator);
+      me = me.creator;
+    }
+
+    while (vampire.creator) {
+      ancestors.push(vampire.creator);
+      vampire = vampire.creator;
+    }
+    console.log(ancestors);
+
+    for (const myAncestor of myAncestors) {
+      for (const ancestor of ancestors) {
+        if (ancestor.name === myAncestor.name) {
+          return ancestor;
+        }
+      }
+    }
   }
 }
 
 module.exports = Vampire;
-
