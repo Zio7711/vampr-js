5fca828392e672e55fea5aa55b2e613f4223bf88
Zio <zi.ao.tan@outlook.com>
2021-05-15 01:38:50 +0000

    Add a function that Returns an array of all the vampires that were converted after 1980

--- vampire.js
+++ vampire.js
@@ -37,6 +37,41 @@ class Vampire {
       this.numberOfVampiresFromOriginal < vampire.numberOfVampiresFromOriginal
     );
   }
+  // Returns the vampire object with that name, or null if no vampire exists with that name
+  vampireWithName(name) {
+    if (name === this.name) {
+      return this;
+    }
+    for (const kid of this.offspring) {
+      const output = kid.vampireWithName(name);
+      if (output) {
+        return output;
+      }
+    }
+    return null;
+  }
+
+  // Returns the total number of vampires that exist
+  get totalDescendents() {
+    let num = 1;
+    for (const kid of this.offspring) {
+      num += kid.totalDescendents;
+    }
+    return num;
+  }
+
+  // Returns an array of all the vampires that were converted after 1980
+  get allMillennialVampires() {
+    let result = [];
+    if (this.yearConverted > 1980) {
+      result.push(this);
+    }
+    for (const kid of this.offspring) {
+      const kidResult = kid.allMillennialVampires;
+      result = result.concat(kidResult)
+    }
+    return result;
+  }
 
   /** Stretch **/
 
@@ -72,3 +107,31 @@ class Vampire {
 }
 
 module.exports = Vampire;
+
+rootVampire = new Vampire('root');
+let offspring1,
+  offspring2,
+  offspring3,
+  offspring4,
+  offspring5,
+  offspring6,
+  offspring7,
+  offspring8;
+offspring1 = new Vampire('a',1981);
+offspring2 = new Vampire('b',1981);
+offspring3 = new Vampire('c',1981);
+offspring4 = new Vampire('d',1981);
+offspring5 = new Vampire('e',1981);
+offspring6 = new Vampire('f',1981);
+offspring7 = new Vampire('g',1000);
+offspring8 = new Vampire('h',1000);
+rootVampire.addOffspring(offspring1);
+rootVampire.addOffspring(offspring2);
+rootVampire.addOffspring(offspring3);
+offspring3.addOffspring(offspring4);
+offspring3.addOffspring(offspring5);
+offspring5.addOffspring(offspring6);
+offspring6.addOffspring(offspring7);
+offspring2.addOffspring(offspring8);
+
+console.log(offspring5.allMillennialVampires);
